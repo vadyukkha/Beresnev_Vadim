7811c5a7ac09a76b343303f9630d342790c56fdf
vadim <vadyukkha@mail.ru>
2023-10-04 23:26:30 +0300

    add class solution with function factorial

--- labTP.cpp
+++ labTP.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class Solution {
+public:
+    int factorial(int x) {
+        if (x == 1) return 1;
+        return x * factorial(x-1);
+    }
+};
+
 int main(){
-    int a = 10, b = 20;
-    cout << a+b << endl;
-    cout << "Good bye!!!" << endl;
+    Solution solve;
+    cout << solve.factorial(5) << endl;
     return 0;
 }
