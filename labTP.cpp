0df2bb15bc1babc9b1e8bfb78995c1360eb63fc0
vadim <144333283+vadyukkha@users.noreply.github.com>
2023-10-04 23:29:31 +0300

    add function sum digits of num

--- labTP.cpp
+++ labTP.cpp
@@ -7,6 +7,14 @@ public:
         if (x == 1) return 1;
         return x * factorial(x-1);
     }
+    int sumDigitsOfNum(int x) {
+        int res = 0;
+        while (x!=0) {
+            res += x%10;
+            x/=10;
+        }
+        return res;
+    }
 };
 
 int main(){
